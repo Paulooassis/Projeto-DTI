a12eed25a85c6c18aa333638a44f83f46838454e
Paulo Assis <passis@sga.pucminas.br>
2025-09-08 18:40:32 -0300

    Delete ecomerce directory

--- ecomerce/Back-End/src/main/java/com/example/ecomerce/exception/ProdutoNaoEncontradoException.java
+++
@@ -1,9 +0,0 @@
-// ProdutoNaoEncontradoException.java
-package com.example.ecomerce.exception;
-
-public class ProdutoNaoEncontradoException extends RuntimeException {
-
-    public ProdutoNaoEncontradoException(Long id) {
-        super("Produto com ID " + id + " não encontrado");
-    }
-}
