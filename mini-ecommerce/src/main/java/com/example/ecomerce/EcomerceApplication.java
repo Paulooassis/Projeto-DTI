74e06658dee20a83ef9f2bec32117ed297b0296d
Pedro Henrique Maia Alves <160970721+PedroMaiaAlves@users.noreply.github.com>
2025-10-05 19:31:44 -0300

    Update EcomerceApplication.java
    Correção de uma mudança errada
    
    Antes: ecommerce
    Agora: ecomerce

--- mini-ecommerce/src/main/java/com/example/ecomerce/EcomerceApplication.java
+++ mini-ecommerce/src/main/java/com/example/ecomerce/EcomerceApplication.java
@@ -1,4 +1,4 @@
-package com.example.ecommerce;
+package com.example.ecomerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
